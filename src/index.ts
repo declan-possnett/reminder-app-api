9eef92817907d8975de2e73aae9d394c0abcbdb4
Declan Possnett <declanpossnett@gmail.com>
2025-11-24 16:58:38 +0000

    add reminders/:id route and catch-all route

--- src/index.ts
+++ src/index.ts
@@ -14,14 +14,35 @@ const pool = new Pool({
 app.use(express.json())
 
 // Routes
+app.get('/reminders/:id', async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`SELECT * FROM reminders WHERE id = $1`, [
+      req.params.id,
+    ])
+
+    if (result.rows.length === 0) {
+      res.status(404).send('Reminder not found')
+    } else {
+      res.json(result.rows[0])
+    }
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('Server error')
+  }
+})
+
 app.get('/reminders', async (req: Request, res: Response) => {
   try {
-    const result = await pool.query(`SELECT * FROM reminders LIMIT 1`)
+    const result = await pool.query(`SELECT * FROM reminders LIMIT 10`)
     res.json(result.rows)
   } catch (error) {
     console.error(error)
     res.status(500).send('Server error')
   }
+})
+
+app.use((req, res) => {
+  res.status(404).json({ error: 'Not Found' })
 })
 
 // Start server
