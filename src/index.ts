f439f1ee5ccf52a1159e33b50ecdc8df697bc840
Declan Possnett <declanpossnett@gmail.com>
2025-12-13 13:07:28 +0000

    update cors config

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,8 @@ app.use(express.json())
 app.use(
   cors({
     origin: ['http://localhost:9000'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   }),
 )
